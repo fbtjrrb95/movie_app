7328012196ff3e38d0a995f57fb8d9f550dd9b2a
seokkyu_ryu <tjrrb95@gmail.com>
2020-09-02 11:15:13 +0900

    Updates

---
+++ precache-manifest.a655451d8fe7699381f34911ce7bddb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294bf5ffe5fb39f649381041edbcadee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8046b56676c3b1faceb1",
+    "url": "/static/css/main.2f4467b9.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8046b56676c3b1faceb1",
+    "url": "/static/js/main.c4ac1662.chunk.js"
+  },
+  {
+    "revision": "e568a14dbad5d9861505",
+    "url": "/static/js/runtime-main.556c272e.js"
+  }
+]);
