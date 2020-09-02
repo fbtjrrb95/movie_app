cbf458cafa98b22f3322e9795a7fcfcb13f5c9e1
seokkyu_ryu <tjrrb95@gmail.com>
2020-09-02 11:16:39 +0900

    Updates

---
+++ precache-manifest.fa84fba9bacdfc78f3c4cad31ed4e744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fefc551edb34cf55bd98d1fd8d6079ba",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8046b56676c3b1faceb1",
+    "url": "/movie_app/static/css/main.2f4467b9.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8046b56676c3b1faceb1",
+    "url": "/movie_app/static/js/main.c4ac1662.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
